03a056df04a7519aed33c8ee08d905d59df57148
Makarew <31836770+Makarew@users.noreply.github.com>
2023-12-07 15:43:26 -0700

    Extract Archives

---
+++ RMMBYLib/Extract.cs
@@ -0,0 +1,26 @@
+﻿using SevenZipExtractor;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMMBYLib
+{
+    public static class Extract
+    {
+        public static string To(string[] data)
+        {
+            try
+            {
+                using (ArchiveFile archive = new ArchiveFile(data[0]))
+                {
+                    archive.Extract(data[1]);
+                }
+
+                return "Success";
+            } catch { return "Error"; }
+        }
+    }
+}
