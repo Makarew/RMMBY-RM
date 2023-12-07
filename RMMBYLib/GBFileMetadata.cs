56291823f1743f3e84c5790e7c992461589013d2
Makarew <31836770+Makarew@users.noreply.github.com>
2023-12-07 15:40:55 -0700

    GameBanana Metadata

---
+++ RMMBYLib/GBFileMetadata.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RMMBYLib
+{
+    public class GBFileMetadata
+    {
+        public FileData[] _aFiles { get; set; }
+
+        public static GBFileMetadata GetMetadata(string modType, string modID)
+        {
+            return JsonConvert.DeserializeObject<GBFileMetadata>(Uri.UnescapeDataString(Client.Get().GetStringAsync(string.Format("https://gamebanana.com/apiv11/{0}/{1}?_csvProperties=_aFiles", modType, modID)).Result));
+        }
+
+        public class FileData
+        {
+            public int _idRow { get; set; }
+            public string _sFile { get; set; }
+            public int _nFilesize { get; set; }
+            public string _sDescription { get; set; }
+            public int _tsDateAdded { get; set; }
+            public int _nDownloadCount { get; set; }
+            public string _sAnalysisState { get; set; }
+            public string _sDownloadUrl { get; set; }
+            public string _sMd5Checksum { get; set; }
+            public string _sClamAvResult { get; set; }
+            public string _sAnalysisResult { get; set; }
+            public bool _bContainsExe { get; set; }
+        }
+    }
+}
---
+++ RMMBYLib/GBSubmissionInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RMMBYLib
+{
+    public class GBSubmissionInfo
+    {
+        public string _sName { get; set; }
+        public Submitter _aSubmitter { get; set; }
+        public Game _aGame { get; set; }
+        public Category _aCategory { get; set; }
+
+
+        public class Submitter
+        {
+            public string _sName { get; set; }
+        }
+        public class Game
+        {
+            public string _sName { get; set; }
+        }
+        public class Category
+        {
+            public string _sName { get; set; }
+        }
+
+        public static GBSubmissionInfo GetMetadata(string modType, string modID)
+        {
+            return JsonConvert.DeserializeObject<GBSubmissionInfo>(Uri.UnescapeDataString(Client.Get().GetStringAsync(string.Format("https://gamebanana.com/apiv11/{0}/{1}/ProfilePage", modType, modID)).Result));
+        }
+    }
+}
