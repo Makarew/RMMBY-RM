ae42af299606ed4e4b7dbaf4ff684f46dcc9a95d
Makarew <31836770+Makarew@users.noreply.github.com>
2023-12-07 15:43:12 -0700

    Added File Locations And Additional Events

--- RMMBYLib/OneClick.cs
+++ RMMBYLib/OneClick.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,18 @@ namespace RMMBYLib
     {
         public event Action UpdateMainText;
         public event Action UpdateLogText;
+        public event Action InstallerFinished;
 
         public string nextMainText;
         public string nextLogText;
+
+        public string downloadedFile;
+        public string destination;
+
+        public virtual async Task<OneClickDownload> StartInstall()
+        {
+            return new OneClickDownload();
+        }
 
         public virtual void StartUp() { }
         public virtual void OnEnd() { }
@@ -27,6 +37,17 @@ namespace RMMBYLib
         {
             nextMainText = text;
             UpdateMainText();
+        }
+
+        protected virtual void UpdateLogAndText(string text)
+        {
+            UpdateLog(text);
+            UpdateTextLine(text);
+        }
+
+        protected virtual void Finished()
+        {
+            InstallerFinished();
         }
     }
 }
